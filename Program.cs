2aea348318f2ebadaab8a66bd4c64cb04893f7ec
LipeClash <felipeclaudino2003@hotmail.com>
2020-06-19 01:30:52 -0300

    feito .

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Sinal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int NumeroDigitado;
+            Console.WriteLine("Leia um número inteiro e exiba “Negativo”, “Zero” ou “Positivo”\n");
+            Console.Write("Digite um número: ");
+
+            NumeroDigitado = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Positivo");
+
+        }  
+    }
+}
